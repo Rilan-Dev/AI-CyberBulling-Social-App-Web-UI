6398006f4a2176dce22e83d43a0240ed310564f7
moham <mohammedrilan.ksa@gmail.com>
2026-03-19 04:21:36 +0530

    code updated

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -189,7 +189,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    return "useAuth must be used within an AuthProvider";
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 }
